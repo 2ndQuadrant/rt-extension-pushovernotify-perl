04098fa10fa92efcd59e8feed4997b441868cad3
Richard Poole <richard@2ndquadrant.com>
2014-05-29 10:18:27 +0100

    version bump to 0.03

--- lib/RT/Extension/PushoverNotify.pm
+++ lib/RT/Extension/PushoverNotify.pm
@@ -10,11 +10,11 @@ RT::Extension::PushoverNotify - integrate Pushover notifications for Request Tra
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
